87f4f251ca90c3b87346a345ed225fde427c0e6c
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-09-17 13:05:49 -0300

    números e operadores

---
+++ 02numeros-e-operadores/script.js
@@ -0,0 +1,63 @@
+var idade = 28;
+console.log(idade);
+
+var gols = 1000;
+console.log(gols);
+
+var pi = 3.4;
+console.log(pi);
+
+var exp = 2e10;
+console.log(exp);
+
+var soma = 77 + 7 + 7;
+console.log(soma);
+
+var subtracao = 77 - 5;
+console.log(subtracao);
+
+var multiplicacao = 70 * 7;
+console.log("Perdoe 70 x 7 = " + multiplicacao);
+
+var divisao = 70 / 7;
+console.log(divisao);
+
+var expoente = 77 ** 7;
+console.log(expoente);
+
+var modulo = 77 % 5;
+console.log(modulo);
+
+// Prestar ateção na ordem das operações
+
+var total1 = 20 + 5 * 2; // 30
+console.log(total1);
+
+var total2 = (20 + 5) * 2; // 50
+console.log(total2);
+
+var total3 = (20 / 2) * 5; // 50
+console.log(total3);
+
+var total4 = 10 + 10 * 2 + 20 / 2; // 40
+console.log(total4);
+
+
+console.log("");
+console.log("Abaixo é incremento : ");
+console.log("");
+
+var incremento = 5
+console.log(incremento++)
+console.log(incremento)
+
+console.log("-------------------");
+
+var incremento = 5
+console.log(++incremento) // ++ Pode ser na frente também
+console.log(incremento)
+
+console.log("-------------------");
+
+var decremento = 5
+console.log(--decremento)
