0dac964da4fba8496e947411cc27a17950ec5a06
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-09-22 17:46:08 -0300

    exercícios

---
+++ 06tudo-e-objeto/exercicios.js
@@ -0,0 +1,28 @@
+console.log("-----------------------");
+console.log("01) Nomeie 3 propriedades ou métodos de strings");
+console.log("-----------------------");
+
+var nome = 'Marcelo'
+console.log(nome.length)
+console.log(nome.toLocaleLowerCase())
+console.log(nome.toString())
+
+console.log("-----------------------");
+console.log("02) Nomeie 5 propriedades ou métodos de elemento DOM");
+console.log("-----------------------");
+/*
+var btn = document.querySelector('.btn')
+btn.addEventListener
+btn.appendChild
+btn.id
+btn.innerHTML
+btn.outerHTML
+
+console.log(btn)
+*/
+
+console.log("-----------------------");
+console.log(
+  "03) Busque na web um objeto (método) capaz de interagir com o clipboard, clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V"
+);
+console.log("-----------------------");
