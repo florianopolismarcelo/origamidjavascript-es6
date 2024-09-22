6c7d75211e0d0f5f4188e95b4c3492b5efe0d8d9
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-09-22 12:41:17 -0300

    tudo é objeto

---
+++ 06tudo-e-objeto/script.js
@@ -0,0 +1,10 @@
+
+var nome = 'Marcelo'
+console.log('A quantidade de caracter no nome Marcelo é = ' + nome.length) // não é uma função pois não tem () é propriedade (retorna quantidade de caracter)
+console.log(nome.toLocaleLowerCase()) // transforma tudo em letra minuscula
+console.log(nome.toLocaleUpperCase()) // transforma tudo em letra maiúscula
+
+
+var altura = 1.75
+
+console.log(altura.toFixed()) // arendonda a altura para 2
