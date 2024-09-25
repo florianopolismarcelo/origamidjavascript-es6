3a496dc4335a3e45eee9c825078eb2a34829c340
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-09-25 13:01:25 -0300

    exercicios

--- 08atribuicao-e-ternario/exercicios.js
+++ 08atribuicao-e-ternario/exercicios.js
@@ -1,60 +1,30 @@
 console.log("-----------------------");
 console.log(
-  "01) Crie um objeto com os seus dados pessoais deve possuir per pelo menos duas propriedades nome e sobre nome"
+  "01) Some 500 ao valor de scroll abaixo, atribuindo o novo valor a scroll"
 );
 console.log("-----------------------");
 
-pessoa = {
-  nome: "Marcelo",
-  sobreNome: "Souza",
-  idade: 28,
-  cidade: "Florianópolis",
-};
-console.log(pessoa);
-
-console.log("-----------------------");
-console.log("02) Crie um método anterior que mostre seu nome completo");
-console.log("-----------------------");
-
-pessoa.nomeCompleto = function () {
-  return `${this.nome} ${this.sobreNome}`;
-};
-console.log(pessoa.nomeCompleto());
-
-console.log("-----------------------");
-console.log("03) Modifique abaixo o valor da propriedade preço para 3000");
-console.log("-----------------------");
-console.log("var carro = {");
-console.log("preco: 1000,");
-console.log("portas: 4,");
-console.log("marca: 'Audi'");
-console.log("}");
-
-var carro = {
-  preco: 1000,
-  portas: 4,
-  marca: "Kind",
-};
-carro.preco = 3000; // Foi mudado sem alterar dentro do objeto
-console.log(carro);
+var scroll = 1000;
+scroll += 500;
+console.log(scroll); //1.500
 
 console.log("-----------------------");
 console.log(
-  "04) Crie um objeto de um cachorro que represente um labrador, preto com 10 anos, que late ao ver um homem"
+  "02) Atribua True para a variável darCredito, caso o cliente possuia carro e casa. E false casa o contrário"
 );
 console.log("-----------------------");
 
-var cachorro = {
-  raca: "Labrador",
-  cor: "Preto",
-  idade: 10,
-  latir(pessoa) {
-    if (pessoa == "homem") {
-      return "latir";
-    } else {
-      return false;
-    }
-  },
-};
-console.log(cachorro)
-console.log(cachorro.latir('homem'))
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+var darCredito = possuiCarro && possuiCasa;
+console.log(darCredito);
+
+// Outra forma fazer abaixo:
+
+var possuiCarro = false;
+var possuiCasa = true;
+var darCredito;
+var darCredito =
+  possuiCarro && possuiCasa ? "Dar crédito ao cliente" : "Negar crédito ao cliente";
+console.log(darCredito);
