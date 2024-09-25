59b26553ae5868280d36e259ba05aba19c355cc1
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-09-25 11:03:55 -0300

    tabuadas

---
+++ 07arrays-e-loops/tabuada.js
@@ -0,0 +1,20 @@
+var numero = 3;
+for (var m = 0; m <= 10; m++) {
+  console.log(numero + " X " + m + " = " + `${numero * m}`);
+}
+
+console.log("-----------------------");
+
+for (var e = 5; e < 0; e++) {}
+for (u = 0; u <= 10; u++) {
+  console.log(e + " X " + u + " = " + `${e * u}`);
+}
+
+console.log("-----------------------");
+
+var numero = 9,
+  i = 0;
+while (i < 10) {
+  i++;
+  console.log(numero + " X " + i + " = " + `${numero * i}`);
+}
