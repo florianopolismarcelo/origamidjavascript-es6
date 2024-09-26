4c093fb81f47179568cb435c2fefb2603cb38187
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-09-26 08:53:43 -0300

    mudança de nome da variável var para let

--- 07arrays-e-loops/tabuada.js
+++ 07arrays-e-loops/tabuada.js
@@ -1,20 +1,22 @@
-var numero = 3;
-for (var m = 0; m <= 10; m++) {
+
+let numero = 3;
+for (let m = 0; m <= 10; m++) {
   console.log(numero + " X " + m + " = " + `${numero * m}`);
 }
 
 console.log("-----------------------");
 
-for (var e = 5; e < 0; e++) {}
+for (e = 5; e < 0; e++) {}
 for (u = 0; u <= 10; u++) {
   console.log(e + " X " + u + " = " + `${e * u}`);
 }
 
+
 console.log("-----------------------");
 
-var numero = 9,
+let a = 9,
   i = 0;
 while (i < 10) {
   i++;
-  console.log(numero + " X " + i + " = " + `${numero * i}`);
+  console.log(a + " X " + i + " = " + `${a * i}`);
 }
