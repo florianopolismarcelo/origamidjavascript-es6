3135af65e13ef0557e7a9d0db4f4e14d0440f5ad
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-10-04 19:57:24 -0300

    exercicios

--- 15dimensoes-e-distancias/exercicios.js
+++ 15dimensoes-e-distancias/exercicios.js
@@ -8,30 +8,6 @@ const imgTop = img.offsetTop;
 console.log(imgTop);
 
 console.log("-----------------------");
-console.log("-----------------------");
-console.log(
-  "03) Verifique se os links da página possuem o mínimo recomendado para telas utilizadas com o dedo, (48px/48px de acordo com o google"
-);
-
-const links = document.querySelectorAll('a')
-links.forEach((link) => {
-  const linkWidth = link.offsetWidth
-  const linkHeight = link.offsetHeight
-  if(linkWidth >= 48 && linkHeight >= 48){
-    console.log(link, "Possui boa acessibilidade")
-  }else{
-    console.log(link, "Não possui boa acessibilidade")
-
-  }
-})
-console.log(links)
-
-console.log("-----------------------");
-console.log(
-  "04) Se o browser for menor que 720px, adicione o clesse menu-mobile ao menu"
-);
-
-console.log("-----------------------");
 console.log("02) Retorne a soma da largura de todas as imagens");
 
 function somaImagens() {
@@ -46,4 +22,30 @@ window.onload = function () {
   somaImagens();
 };
 
+console.log("-----------------------");
+console.log(
+  "03) Verifique se os links da página possuem o mínimo recomendado para telas utilizadas com o dedo, (48px/48px de acordo com o google"
+);
 
+const links = document.querySelectorAll("a");
+links.forEach((link) => {
+  const linkWidth = link.offsetWidth;
+  const linkHeight = link.offsetHeight;
+  if (linkWidth >= 48 && linkHeight >= 48) {
+    console.log(link, "Possui boa acessibilidade");
+  } else {
+    console.log(link, "Não possui boa acessibilidade");
+  }
+});
+console.log(links);
+
+console.log("-----------------------");
+console.log(
+  "04) Se o browser for menor que 720px, adicione o clesse menu-mobile ao menu"
+);
+
+const browserSmall = window.matchMedia("(max-width: 720px)").matches;
+if (browserSmall) {
+  const menu = document.querySelector(".menu");
+  menu.classList.add("menu-mobile");
+}
