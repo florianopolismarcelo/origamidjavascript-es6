18c1e8a41ec4a01fd155d27798b246acafab506a
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-10-14 16:31:39 -0300

    constructor function

--- 22objetos/23constructor-functions/exercicios.js
+++
@@ -1,5 +0,0 @@
-console.log("-----------------------");
-console.log(
-  "01) Crie um array com os anos que o Brasil ganhou a copa 1959, 1962, 1970, 1994, 2002"
-);
-console.log("-----------------------");
