69ce8156711e9bf6bcf41ec0fc0c02eca31a4efa
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-10-25 08:22:46 -0300

    exercicios

--- 31object/exercicios.js
+++ 31object/exercicios.js
@@ -1,5 +1,18 @@
 console.log("-----------------------");
+console.log("01) Crie uma função que verifique corretamente o tipo de dado ");
+
+console.log("-----------------------");
 console.log(
-  "01) "
+  "02) / Crie um objeto quadrado com a propriedade lados e torne ela imutável Previna qualquer mudança no objeto abaixo "
 );
 
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: "#333",
+};
+
+console.log("-----------------------");
+console.log(
+  "03) Liste o nome de todas as propriedades do protótipo de String e Array "
+);
