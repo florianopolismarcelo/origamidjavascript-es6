f0548fe07abe8b1fcecb763173797adf062ac28a
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-10-31 19:34:35 -0300

    exercicio settimeout e setinterval

--- 37settimeout-e-setinterval/exercicios.js
+++ 37settimeout-e-setinterval/exercicios.js
@@ -1,2 +1,45 @@
-console.log("01) ");
+console.log(
+  "01) Mude a cor da tela para azul e depois para vermelho a cada 2s"
+);
 console.log("-----------------------");
+
+/*
+function mudarClasse() {
+  document.body.classList.toggle("active");
+}
+*/
+
+console.log(
+  "02) Crie um cronometro utilizando o setInterval. Deve ser possível iniciar, pausar e resetar (duplo clique no pusar)."
+);
+console.log("-----------------------");
+
+const iniciar = document.querySelector(".iniciar");
+const pausar = document.querySelector(".pausar");
+const tempo = document.querySelector(".tempo");
+
+iniciar.addEventListener("click", iniciarTempo);
+pausar.addEventListener("click", pausarTempo);
+pausar.addEventListener("dblclick", resetarTempo);
+
+let i = 0;
+let timer;
+
+function iniciarTempo() {
+  timer = setInterval(() => {
+    tempo.innerText = i++;
+  }, 100);
+  iniciar.setAttribute("disabled", "");
+}
+
+function pausarTempo() {
+  clearInterval(timer);
+  iniciar.removeAttribute("disabled");
+  //console.log("pausar");
+}
+
+function resetarTempo() {
+  tempo.innerText = 0;
+  i = 0;
+  //console.log("resetar");
+}
