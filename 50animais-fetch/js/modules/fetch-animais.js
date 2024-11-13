f1d0d9b08c36edc32f4c7c78c40da1abfe12bbe1
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-11-13 15:32:26 -0300

    animais fetch e bitcoin fetch

--- 50animais-fetch/js/modules/fetch-animais.js
+++ 50animais-fetch/js/modules/fetch-animais.js
@@ -2,6 +2,7 @@ import initAnimaNumeros from "./anima-numeros.js";
 
 export default function initFetchAnimais() {
   async function fetchAnimais(url) {
+    try {
     const animaisResponse = await fetch(url);
     const animaisJSON = await animaisResponse.json();
     const numerosGrid = document.querySelector(".numeros-grid");
@@ -12,7 +13,10 @@ export default function initFetchAnimais() {
       //console.log(divAnimal);
     });
     initAnimaNumeros();
+  }catch(erro) {
+    console.log(erro)
   }
+}
   function createAnimal(animal) {
     const div = document.createElement("div");
     div.classList.add("numero-animal");
