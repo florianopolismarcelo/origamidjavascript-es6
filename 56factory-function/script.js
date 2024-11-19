abe29bcc8561043b9d7644f88f5b975d7931f4a6
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-11-19 08:56:27 -0300

    factory function

--- 56factory-function/script.js
+++ 56factory-function/script.js
@@ -22,31 +22,94 @@ function createButton2(text) {
     buttonElement.innerText = text;
     return buttonElement;
   }
-  return Object.freeze({ // Não deixa sobrescrever outro nome congela (fixa) botão Comprar e Vender
+  return Object.freeze({
+    // Não deixa sobrescrever outro nome congela (fixa) botão Comprar e Vender
     text,
-    element
+    element,
   });
 }
 const btnCompar2 = createButton2("Comprar");
 const btnVender2 = createButton2("Vender");
 
-//btnCompar2.text = 'Novo texto';  
+//btnCompar2.text = 'Novo texto';
 //btnVender2.element = 'Novo texto';
 
 console.log(btnCompar2, btnVender2);
 
 //--------------------------------------------------
 
-function Pessoa(nome){
-  if(!(this instanceof Pessoa)){
-    return new Pessoa(nome)
+function Pessoa(nome) {
+  if (!(this instanceof Pessoa)) {
+    return new Pessoa(nome);
   }
   this.nome = nome;
 }
-Pessoa.prototype.andar = function() {
-  return `${this.nome} andou`
-}
-const designer = Pessoa('André')
-console.log(designer)
+Pessoa.prototype.andar = function () {
+  return `${this.nome} andou`;
+};
+const designer = Pessoa("André");
+console.log(designer);
 
 //--------------------------------------------------
+
+console.log("Abaixo continuação aula:");
+// Observação: $$ é sinal de JQuery
+
+function $$(selectedElements) {
+  const elements = document.querySelectorAll(selectedElements);
+
+  function hide() {
+    elements.forEach((element) => {
+      element.style.display = "none";
+    });
+    return this; // esse retorno com this funciona da mesma forma do retorno comentado abaixo:
+    //return $$(selectedElements);
+  }
+  function show() {
+    elements.forEach((element) => {
+      element.style.display = "initial";
+    });
+    return this;
+    //return $$(selectedElements);
+  }
+
+  function on(onEvent, callback) {
+    elements.forEach((element) => {
+      element.addEventListener(onEvent, callback);
+    });
+    return this;
+    //return $$(selectedElements);
+  }
+
+  function addClass(className) {
+    elements.forEach((element) => {
+      element.classList.add(className);
+    });
+    return this;
+    //return $$(selectedElements);
+  }
+  function removeClass(className) {
+    elements.forEach((element) => {
+      element.classList.remove(className);
+    });
+    return this;
+    //return $$(selectedElements);
+  }
+
+  return {
+    elements,
+    hide,
+    show,
+    on,
+    addClass,
+    removeClass,
+  };
+}
+const btns = $$("button");
+console.log(btns.hide().show());
+
+function handleClick(event) {
+  console.log(event.target);
+  btns.addClass("active");
+}
+btns.on("click", handleClick);
