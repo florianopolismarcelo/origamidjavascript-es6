93b930fa9d40c4543844ca87a0aab687d2e148d4
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-11-21 13:09:37 -0300

    rest e spread + exercicios

---
+++ 59rest-e-spread/exercicios.js
@@ -0,0 +1,42 @@
+console.log(
+  "01) Reescreva a função utilizando valores iniciais de parâmetros com ES6"
+);
+/*
+function createButton(background, color) {
+  background = background || "blue";
+  if (color === undefined) {
+    color = "red";
+  }
+  const buttonElement = document.createElement("button");
+  buttonElement.style.background = background;
+  buttonElement.style.color = color;
+  return buttonElement;
+}
+
+const redButton = createButton();
+* */
+function createButton(background, color = "red") {
+  const buttonElement = document.createElement("button");
+  buttonElement.style.background = background;
+  buttonElement.style.color = color;
+  return buttonElement;
+}
+const redButton = createButton();
+console.log(redButton);
+
+console.log("-----------------------");
+
+console.log(
+  "02) Utilize o método push para inserir as frutas ao final de comidas."
+);
+/**
+const frutas = ["Banana", "Uva", "Morango"];
+const comidas = ["Pizza", "Batata"];
+ */
+
+const frutas = ["Banana", "Uva", "Morango"];
+const comidas = ["Pizza", "Batata"];
+
+comidas.push(...frutas);
+
+console.log(comidas);
